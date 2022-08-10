d95e4019064f1df9a9001289d17b628374db422b
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-08-10 14:46:02 -0300

    :wrench: update authcontroller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,15 +9,14 @@ import { getUserByEmail } from "../repositories/usersRepository.js";
 
 export async function login(req, res) {
   const { email, password } = req.body;
+
   const { rows: users } = await getUserByEmail(email);
   const [user] = users;
   if (!user) {
     return res.sendStatus(401);
   }
-  if (
-    bcrypt.compareSync(password, user.password) &&
-    !getSessionUserId(user.id)
-  ) {
+  const session = await getSessionUserId(user.id);
+  if (bcrypt.compareSync(password, user.password) && !session) {
     const token = uuid();
     await createSession(token, user.id);
     console.log(user);
