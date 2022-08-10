f28b3a7d260787ab956291a32829f1f91857cac5
Danton Matheus <dantoncosta03@hotmail.com>
2022-08-10 11:23:24 -0300

    :memo: Corrige o controller chamado na rota de logout

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 // import userSchema from "../schemas/userSchema.js";
 import loginSchema from "./../schemas/loginSchema.js";
-import { login } from "./../controllers/authController.js";
+import { login, logout } from "./../controllers/authController.js";
 // import { createUser } from "../controllers/userController.js";
 import { validateSchema } from "./../middlewares/schemaValidator.js";
 import authenticateToken from "./../middlewares/authenticateToken.js";
@@ -10,6 +10,6 @@ const authRouter = Router();
 
 // authRouter.post('/signup', validateSchema(userSchema), createUser);
 authRouter.post("/signin", validateSchema(loginSchema), login);
-authRouter.put("/logout", authenticateToken, login);
+authRouter.put("/logout", authenticateToken, logout);
 
 export default authRouter;
