069b6b809695a34e2d3aca200741884ed062f25d
josimar120 <josimar.17perseu@gmail.com>
2022-08-10 22:04:19 -0300

    refactor:(mesclando arquivo duplicado)

--- src/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import signUp from "../controllers/authController.js";
-import validateSignUp from "../middlewares/validateSignUp.js";
-
-const authRouter = Router();
-
-authRouter.post('/signup', validateSignUp, signUp);
-
-export {authRouter};
