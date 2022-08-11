38ac755c0e28db6a793f64e6ef86cb2d9662cbd0
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-08-11 01:21:29 -0300

    test logout

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -4,26 +4,18 @@ import { getSession } from "../repositories/authRepository.js";
 dotenv.config();
 
 export default async function authenticateToken(req, res, next) {
-  console.log(req.headers);
-  const { authorization } = req.headers;
-  const token = authorization?.split(" ")[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) {
-    console.log(token, "token");
-    return res.sendStatus(401);
-  }
+  if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
-    if (err) {
-      console.log(err, "error");
-      return res.sendStatus(401);
-    }
+    if (err) return res.sendStatus(401);
     req.email = user;
   });
 
   const session = await getSession(token);
   if (!session) {
-    console.log(session, "session");
     return res.sendStatus(401);
   }
 
