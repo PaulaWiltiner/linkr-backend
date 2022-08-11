1917ec9ed14c40c2db4fa72ebbb5255f346474b0
josimar120 <josimar.17perseu@gmail.com>
2022-08-11 02:50:05 -0300

    fix:(correção no nome do joi)

--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -4,6 +4,6 @@ export const signupSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
     confirmPassword: Joi.ref('password'),
-    username: Joi.string().required(),
+    userName: Joi.string().required(),
     picture: Joi.string().uri().required()
 });
