ef4a21e57519801c288cafea740da917333d751a
josimar120 <josimar.17perseu@gmail.com>
2022-08-12 20:43:30 -0300

    feat:(resolvendo conflito)

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { validateCreatePost } from "../middlewares/validateCreatePost.js";
-import {validateUpdatePost} from "../middlewares/validateUpdatePost.js";
 import {createPost, deletePost,  pullPosts, updatePost} from "../controllers/postsController.js";
 import { validateAlterationPost } from "../middlewares/validateAlterationPost.js";
+import { validateCreatePost } from "../middlewares/validateCreatePost.js";
+import {validateUpdatePost} from "../middlewares/validateUpdatePost.js";
 import authenticateToken from "../middlewares/authenticateToken.js";
 
 export const postsRouter = Router();
 postsRouter.post("/post", authenticateToken, validateCreatePost, createPost);
 postsRouter.get("/posts", pullPosts);
-postsRouter.put("/post/:id", authenticateToken, validateAlterationPost, validateUpdatePost, updatePost);
+postsRouter.put("/post/:id", authenticateToken,validateUpdatePost, validateAlterationPost, updatePost);
 postsRouter.delete("/posts/:id",  authenticateToken,  validateAlterationPost,  deletePost);
