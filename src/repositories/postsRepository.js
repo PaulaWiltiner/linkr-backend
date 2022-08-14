912b157135c80d91a80247622c78cb143b27af6c
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-08-14 17:36:25 -0300

    Filtering posts by username

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -23,6 +23,30 @@ export async function getPosts(userId) {
 
   return posts;
 }
+export async function getPostsByUsername(userId, username) {
+  const { rows: posts } = await connection.query(
+    `
+    SELECT  posts.id, json_build_object('description',posts."descriptionurl",'title',posts."titleurl",'url',posts."link", 'image',posts."imageurl") AS link,
+     posts.description, json_build_object('id',users.id ,'username',users.username, 'picture',users.picture) AS user,
+                       (SELECT json_build_object('count',COUNT("pL".id) ,
+												 'usernameList' , (array_agg(users.username))[1:3] ,
+												 'isLiked' , (json_agg(COALESCE(("pL"."userId" = $1), 'false')) FILTER (WHERE COALESCE(("pL"."userId" = $1), 'false'))) )
+                        FROM 
+						(SELECT * 
+						FROM "postLikes"  
+						ORDER BY "createdAt" DESC) AS "pL"
+     JOIN users ON "pL"."userId"=users.id WHERE "pL"."postId"= posts.id) AS "postLikes"
+     FROM posts
+     JOIN "userPosts" ON "userPosts"."postId"=posts.id
+     JOIN users ON "userPosts"."userId"=users.id
+     WHERE users.username = $2
+     ORDER BY posts.id DESC LIMIT 20;
+  `,
+    [userId, username]
+  );
+
+  return posts;
+}
 export async function getPostById(id) {
   const { rows: post } = await connection.query(
     `SELECT * FROM "userPosts" WHERE "postId" = $1;`,
