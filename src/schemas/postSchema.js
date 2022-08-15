ce29876a16515ce91b102ebb7a2e6ad5156d4fd4
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-08-15 13:49:02 -0300

    permiting post description to be empty

--- src/schemas/postSchema.js
+++ src/schemas/postSchema.js
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 export const createPostSchema = Joi.object({
-  description: Joi.string().required(),
+  description: Joi.string(),
   link: Joi.string().uri().required(),
 });
 
 export const updateSchema = Joi.object({
-  description: Joi.string().required()
+  description: Joi.string().required(),
 });
