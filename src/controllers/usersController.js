32d7b9ab529bfcfd2b9527cb3f36bc0e20403290
Danton Matheus <dantoncosta03@hotmail.com>
2022-08-16 16:22:26 -0300

    :memo: Cria a feat de pesquisa por usuário

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,7 @@
-import { getAllUsers } from "../repositories/usersRepository.js";
+import { searchUsers } from "../repositories/usersRepository.js";
 
 export async function getUsers(req, res) {
-  const { rows: users } = await getAllUsers();
-
+  const { username } = req.params;
+  const { rows: users } = await searchUsers(username);
   return res.status(200).send(users);
 }
