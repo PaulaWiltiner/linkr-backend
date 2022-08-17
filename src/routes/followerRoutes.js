47bf9297682c3ab4a27f750bf3a057b17f1e0bcc
josimar120 <josimar.17perseu@gmail.com>
2022-08-16 21:07:19 -0300

    feat:(criação das rotas de seguidores)

---
+++ src/routes/followerRoutes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import {followed, unfollowed} from "../controllers/followerControllers.js";
+
+export const followerRouter = Router();
+
+followerRouter.post('user/follower/:id', followed() );
+followerRouter.delete('user/follower/:id', unfollowed());
