c53f0047b0c6579960989b5d2edf28f1474de968
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-08-19 02:21:16 -0300

    :memo: correction rePost

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -39,7 +39,7 @@ export async function getPostsByUserId(userId) {
 						(SELECT * 
 						FROM "postLikes"  
 						ORDER BY "createdAt" DESC) AS "pL"
-     JOIN users ON "pL"."userId"=users.id WHERE "pL"."postId"= posts.id) AS "postLikes"
+     JOIN users ON "pL"."userId"=users.id WHERE "pL"."postId"= posts.id) AS "postLikes", posts."isRepost"
      FROM posts
      JOIN "userPosts" ON "userPosts"."postId"=posts.id
      JOIN users ON "userPosts"."userId"=users.id
